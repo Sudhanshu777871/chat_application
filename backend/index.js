3cc093c2c71770bd881efa4ca02fd5cc2945e00d
Sudhanshu <ksudhanshu394@gmail.com>
2024-03-29 06:50:24 +0530

    project completed 1.0

--- backend/index.js
+++ backend/index.js
@@ -3,102 +3,200 @@ const app = express();
 const cors = require("cors");
 const bcrypt = require('bcryptjs');
 const config = require("./config");
+let nameSpace = '';
 require("dotenv").config();
+// code for socket.io
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
 // applying the middleware
 app.use(express.json());
 app.use(cors());
 
+// code for creating the namespace
+io.on("create_namespace", (data) => {
+  nameSpace = io.of(`/${data}`)
+})
+// CODE FOR SOCKET.IO
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.emit('test', "A Message From Backend Of Code");
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+
+// CODE FOR MAKING API
 // CREATING THE LOGIN API
 app.post("/login", (req, res) => {
-    if (req.body.email && req.body.password) {
-      const { email, password } = req.body;
-  
-      // Query the database to find the user by phone
-      config.query(
-        "SELECT * FROM user WHERE Email = ?",
-        [email],
-        async (error, results) => {
-          if (error) {
-            throw error;
-          } else if (results.length === 0) {
-            res.status(401).send({ errMsg: "User not found" });
-          } else {
-            const user = results[0];
-            const storedPassword = user.Password;
-  
-            // Compare the provided password with the hashed password
-            try {
-              const passwordMatch = await bcrypt.compare(password, storedPassword);
-  
-              if (passwordMatch) {
-                res.send(true);
-              } else {
-                res.status(401).send({ errMsg: "Invalid password" });
-              }
-            } catch (err) {
-              console.error(err);
-              res.status(500).send({ errMsg: "Login failed" });
+  if (req.body.email && req.body.password) {
+    const { email, password } = req.body;
+
+    // Query the database to find the user by phone
+    config.query(
+      "SELECT * FROM user WHERE Email = ?",
+      [email],
+      async (error, results) => {
+        if (error) {
+          throw error;
+        } else if (results.length === 0) {
+          res.status(401).send({ errMsg: "User not found" });
+        } else {
+          const user = results[0];
+          const storedPassword = user.Password;
+
+          // Compare the provided password with the hashed password
+          try {
+            const passwordMatch = await bcrypt.compare(password, storedPassword);
+
+            if (passwordMatch) {
+              res.send(true);
+            } else {
+              res.status(401).send({ errMsg: "Invalid password" });
             }
+          } catch (err) {
+            console.error(err);
+            res.status(500).send({ errMsg: "Login failed" });
           }
         }
-      );
-    } else {
-      res.status(400).send({ errMsg: "Please Enter Both Email and Password" });
-    }
-  });
+      }
+    );
+  } else {
+    res.status(400).send({ errMsg: "Please Enter Both Email and Password" });
+  }
+});
 
 // code for api for signup
 app.post("/account", (req, res) => {
-    if (req.body.username && req.body.email && req.body.password) {
-        const { username, email, password } = req.body;
+  if (req.body.username && req.body.email && req.body.password) {
+    const { username, email, password } = req.body;
 
-        // Check if the phone number is already registered
-        config.query(
-            "SELECT * FROM user WHERE Email = ?",
-            [email],
-            async (error, results) => {
-                if (error) {
-                    throw error;
-                } else if (results.length > 0) {
-                    res.status(400).send({ errMsg: "Email Already Exits" });
-                } else {
-                    // Hash the password
-                    const hashedPassword = await bcrypt.hash(password, 10);
+    // Check if the phone number is already registered
+    config.query(
+      "SELECT * FROM user WHERE Email = ?",
+      [email],
+      async (error, results) => {
+        if (error) {
+          throw error;
+        } else if (results.length > 0) {
+          res.status(400).send({ errMsg: "Email Already Exits" });
+        } else {
+          // Hash the password
+          const hashedPassword = await bcrypt.hash(password, 10);
 
-                    // Insert the new user into the database
-                    config.query(
-                        "INSERT INTO user (UserName, Email, Password) VALUES (?, ?, ?)",
-                        [username, email, hashedPassword],
-                        (error) => {
-                            if (error) {
-                                throw error;
-                            }
-                            res.send(true);
-                        }
-                    );
-                }
+          // Insert the new user into the database
+          config.query(
+            "INSERT INTO user (UserName, Email, Password) VALUES (?, ?, ?)",
+            [username, email, hashedPassword],
+            (error) => {
+              if (error) {
+                throw error;
+              }
+              res.send(true);
             }
-        );
-    } else {
-        res.status(400).send({ errMsg: "Please Enter Name, Email, and Password" });
-    }
+          );
+        }
+      }
+    );
+  } else {
+    res.status(400).send({ errMsg: "Please Enter Name, Email, and Password" });
+  }
 });
 
 // CODE FOR VALIDATING THE USEREMAIL FOR NEW CHAT 
 app.post("/email_validate", (req, res) => {
-  if(req.body.email){
-    const {email} = req.body;
+  if (req.body.email) {
+    const { email } = req.body;
     config.query(
-      "SELECT Email FROM user WHERE Email=?",[email],
+      "SELECT Email FROM user WHERE Email=?", [email],
       (error, result) => {
         if (error) throw error;
         res.send(result);
       }
     );
   }
-  else{
+  else {
     res.send("Please Send The Email")
   }
 });
+
+// code for making API for inserting the json data to connected data
+app.post("/insert_connected_people", (req, res) => {
+  if (req.body.adminEmail && req.body.connectedEmail) {
+    const { adminEmail, connectedEmail } = req.body;
+    // code for getting the name of connected user using email
+    config.query('SELECT UserName FROM user WHERE Email=?', [connectedEmail], (error, result) => {
+      if (error) {
+        res.status(500).send('Some Error During Fetching Of User Name');
+        return;
+      } else {
+        console.log(result[0].UserName)
+        // code for inserting the connected user data
+        config.query("INSERT INTO peopleconnect (adminEmail, connectedPersonEmail, connectedPersonName) VALUES (?,?,?)", [adminEmail, connectedEmail, result[0].UserName], (err) => {
+          if (err) {
+            res.status(500).send("Some, Please Try Again...");
+            return;
+          } else {
+            res.status(200).send(true);
+          }
+        });
+      }
+
+    })
+
+  } else {
+    res.status(400).send("Please send the Admin Email, Connected Email, and Connected Name");
+  }
+});
+
+// code for making API for showing all the users
+app.post("/show_all_users", (req, res) => {
+  if (req.body.userEmail) {
+    const { userEmail } = req.body;
+    // code for showing all connected users
+    config.query("SELECT connectedPersonEmail, connectedPersonName FROM peopleconnect WHERE adminEmail=?", [userEmail], (err, result) => {
+      if (err) {
+        res.status(500).send("Some, error occures, please try again...");
+        return;
+      } else {
+        res.status(200).send(result);
+      }
+    })
+  }
+  else {
+    res.status(400).send("Please Send The User Name  Of User");
+  }
+})
+
+// code for making api for inserting the chat msg
+app.post("/insert_msg", (req, res) => {
+  if (req.body.senderEmail && req.body.reciverEmail && req.body.msg) {
+    const { senderEmail, reciverEmail, msg } = req.body;
+    config.query(
+      "INSERT INTO master_msg (senderEmail,reciverEmail,msg) VALUES (?,?,?)",
+      [senderEmail, reciverEmail, msg],
+      (error, result) => {
+        if (error) {
+          res.status(500).send("Some, error occures, please try again...");
+          return;
+        } else {
+          res.status(200).send(result);
+        }
+      }
+    );
+  }
+  else {
+    res.status(400).send("Please Send The Send Email, Reciver Email and Msg");
+  }
+});
+
+
 // Listening The App
-app.listen(process.env.RUNNING_PORT);
+http.listen(process.env.RUNNING_PORT);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import './styles/Home.css'
+import socket from './Socket';
 import { useNavigate } from 'react-router-dom'
 import peopeImg from './img/people.png'
 import welcomeImg from './img/welcome.png'
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import './styles/Home.css'
+import { v4 as uuidv4 } from 'uuid';
 function Home() {
+
     // code for accessing the navigate
     const navigate = useNavigate();
     // useState for toggle welcome message
@@ -14,7 +18,11 @@ function Home() {
     const [userEmailAddPeople, setUserEmailAddPeople] = useState('');
 
     // usesate for setmsg user is available or not
-    const [userFound, setUserFound] = useState(true);
+    const [userFoundChatBtn, setUserFoundChatBtn] = useState(true);
+    const [userFoundMsg, setUserFoundMsg] = useState('');
+    const [connectedUserResult, setConnectedUserResult] = useState([]);
+    // usestate for showing people name is main message container
+    const [userNameMainMsg, setUserNameMainMsg] = useState('');
 
     // function for logout
 
@@ -22,16 +30,51 @@ function Home() {
         localStorage.clear();
         navigate('/login');
     }
+    // code for notify
+    const notify = (msg) =>
+        toast.error(msg, {
+            position: "bottom-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
 
     // function for add people chat
     const addPeopleChat = async () => {
+        // code for adding all connected people
+        let addPeople = await fetch('http://localhost:3500/insert_connected_people', {
+            method: "POST",
+            body: JSON.stringify({ adminEmail: localStorage.getItem("userEmail"), connectedEmail: userEmailAddPeople }),
+            headers: { "Content-Type": "application/json" },
+        });
 
+        if (addPeople) {
+            addPeople = await addPeople.json();
+
+            if (addPeople === true) {
+                showConnectedUsers()
+                setUserEmailAddPeople()
+            }
+            else {
+                notify("Some Error Occured, Please Try Again...")
+            }
+        }
     }
 
     // function for show Message
 
-    const userMessageShownFun = () => {
+    const userMessageShownFun = (userName, userEmail) => {
         setToggleWelcome(false);
+        setUserNameMainMsg(userName)
+        // code for creating the nameserver
+        socket.on('connect', () => {
+            // code for emitting the message
+            socket.emit("create_namespace", {namespace_name:userEmail+localStorage.getItem("userEmail")})
+        })
     }
 
     // function for validating the add email user
@@ -47,22 +90,56 @@ function Home() {
             login = await login.json();
 
             if (login.length > 0) {
-                setUserFound(false)
-            } else {
-                setUserFound(true)
+
+                if (login[0].Email === localStorage.getItem("userEmail")) {
+                    setUserFoundMsg("This is your own email, Try Different");
+                    setUserFoundChatBtn(true);
+                }
+
+                else {
+                    setUserFoundMsg("User Found");
+                    setUserFoundChatBtn(false);
+                }
+            }
+            else {
+                setUserFoundMsg("User Not Found");
+                setUserFoundChatBtn(true);
             }
         }
 
     }
-    // code for useeffect
-    useEffect(() => {
-        const auth = localStorage.getItem("userEmail");
-        if (!auth) {
-            navigate("/login");
+    // CODE FOR SHOWING ALL THE CONNECTED USERS 
+    const showConnectedUsers = async () => {
+        let getApi = await fetch("http://localhost:3500/show_all_users", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ userEmail: localStorage.getItem("userEmail") }),
+        });
+        if (getApi) {
+            getApi = await getApi.json();
+            if (getApi.length > 0) {
+                setConnectedUserResult(getApi)
+            } else {
+                console.log("Some Error Ocuured....")
+            }
         }
-    }, [navigate])
+
+    }
+    // function for showing all users
+    useEffect(() => {
+        const auth = localStorage.getItem('userEmail');
+        if (!auth) {
+            navigate('/login');
+        } else {
+            showConnectedUsers()
+        }
+
+    }, [navigate]);
+
+
     return (
         <>
+
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-3 chatPeople" style={{ padding: '0px', borderRight: '2px solid black' }}>
@@ -85,13 +162,21 @@ function Home() {
 
                         {/* code for chat people */}
                         <div className="mainDivPeopleContact">
+                            {connectedUserResult.map((data, index) => {
+                                return (
+                                    <>
+                                        <div className="peopleIdDiv" onClick={() => { userMessageShownFun(data.connectedPersonName, data.connectedPersonEmail) }} key={uuidv4()}>
+                                           
+                                            <img src={peopeImg} alt="people" className='peopleStyle' />
+                                            <h1 className='peopleNameTitle'>{data.connectedPersonName}</h1>
+                                        </div>
+                                        <hr style={{ border: '1px solid grey', margin: "0px 9px" }} />
+
+                                    </>
+                                )
+                            })}
 
 
-                            <div className="peopleIdDiv" onClick={() => { userMessageShownFun() }}>
-                                <img src={peopeImg} alt="people" className='peopleStyle' />
-                                <h1 className='peopleNameTitle'>Sudhanshu Kumar</h1>
-                            </div>
-                            <hr style={{ border: '1px solid grey', margin: "0px 9px" }} />
                         </div>
 
                     </div>
@@ -104,7 +189,7 @@ function Home() {
                         {/* CODE FOR SEND MESSAGE AND OTHER THINGS PROFILE */}
 
                         {!toggleWelcome && <><div className="mainHeader" style={{ paddingLeft: '20px', paddingRight: '20px' }}>
-                            <div style={{ display: 'flex' }}><p id='profileName'>S</p> <h3 style={{ color: "white", fontSize: "15px", marginTop: "28px", marginLeft: "11px" }}>Sudhanshu Kumar</h3></div>
+                            <div style={{ display: 'flex' }}><p id='profileName'>S</p> <h3 style={{ color: "white", fontSize: "15px", marginTop: "28px", marginLeft: "11px" }}>{userNameMainMsg}</h3></div>
 
                             {/* code for three dots */}
                             <div className="dropdown">
@@ -118,6 +203,11 @@ function Home() {
                                 </ul>
                             </div>
                         </div>
+                            <div className="divMsgShow">
+                                <p className='msgStyleTxtP senderMsg'>Hello</p>
+                                <br />
+                            </div>
+
                             {/* code for bottom send message */}
                             <div className="sendBottomMsg">
                                 <input type="text" id="typeMsgUserStyle" placeholder='Type a message' autoComplete='off' />
@@ -148,14 +238,15 @@ function Home() {
                             </form>
                         </div>
                         <div className="modal-footer" style={{ backgroundColor: 'rgb(32,44,51)' }}>
-                            <p style={{ color: "white", textAlign: 'start' }}>{userFound === false ? "" : "User Not Found"}</p>
+                            <p style={{ color: "white", textAlign: 'start' }}>{userFoundMsg}</p>
                             <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
 
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" disabled={userFound} onClick={addPeopleChat}>Start Chat  <i className="fa fa-comment"></i></button>
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" disabled={userFoundChatBtn} onClick={addPeopleChat}>Start Chat  <i className="fa fa-comment"></i></button>
                         </div>
                     </div>
                 </div>
             </div>
+            <ToastContainer />
         </>
     )
 }
